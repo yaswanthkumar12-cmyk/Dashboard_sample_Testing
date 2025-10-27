7b05c652f9153ba638ca8dc97cc35a2b91a7c38e
Yaswanth <Yaswanth@middlewaretalents.com>
2025-10-27 14:28:36 +0530

    Prepare release v1

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
 
   return (
     <Container fluid className="p-4">
-      <h1 className="mb-4">Dashboard_sample_Testing</h1>
+      <h1 className="mb-4">Dashboard_sample_Testing_v1</h1>
 
       {/* Stats */}
       <Row className="mb-4">
